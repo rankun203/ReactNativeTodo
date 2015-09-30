6b9fc11dff76e93a4e8d6623c1c9feb578c3f9ed
rankun203 <rankun203@gmail.com>
2015-09-30 17:23:21 +0800

    ADD: movie, first look

--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,82 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View
+    } = React;
 
-var reactNativeTodo = React.createClass({
-  render: function() {
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}}
+];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var App = React.createClass({
+  getInitialState: function () {
+    return {movies: null};
+  },
+  fetchData: function () {
+    fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseData)=> {
+          this.setState({
+            movies: responseData.movies
+          });
+        }).done();
+    this.setState({movies: []});
+  },
+  componentDidMount: function () {
+    this.fetchData();
+  },
+  renderLoadingView: function () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <Text>Loading movies...</Text>
+        </View>
     );
+  },
+  renderMovie: function (movie) {
+    return (
+        <View style={styles.container}>
+          <Image
+              source={{uri: movie.posters.thumbnail}}
+              style={styles.thumbnail}
+              />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
+    );
+  },
+  render: function () {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
   }
 });
 
-var styles = StyleSheet.create({
+var styles = {
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    flexDirection: 'row'
+  }, thumbnail: {
+    width: 53,
+    height: 81
+  }, rightContainer: {
+    flex: 50,
+    alignItems: 'center'
+  }
+};
 
-AppRegistry.registerComponent('reactNativeTodo', () => reactNativeTodo);
+AppRegistry.registerComponent('reactNativeTodo', () => App);
